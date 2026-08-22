f3c6629ef2d02b8f09f105d0e120eacd7748ff98
DavitBarnabishvili <dbarn21@freeuni.edu.ge>
2026-08-22 04:40:48 +0400

    add swagger/OpenAPI dependency and config class

---
+++ src/main/java/com/arcticblu/subscriptioncapacityallocator/config/OpenApiConfig.java
@@ -0,0 +1,11 @@
+package com.arcticblu.subscriptioncapacityallocator.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Subscription Allocator API", version = "v1")
+)
+public class OpenApiConfig {}
